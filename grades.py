76daa2004d35758b3a9fc207127aeb45a78a13ea
Damien Christian <damienchristian987@gmail.com>
2025-09-03 13:35:04 -0500

    Add grades.py

---
+++ grades.py
@@ -0,0 +1,24 @@
+def average_score(scores: list[int]) -> float:
+    total = 0
+    for sc in scores:
+        total = total + sc
+    return total / len(scores)
+
+def pass_fail(score: int) -> str:
+    if score > 60:
+        return "pass"
+    else:
+        return "fail"
+
+def analyze_scores(scores: list[int]) -> None:
+    for sc in scores:
+        print(f"{sc}: {pass_fail(sc)}")
+
+if __name__ == "__main__":
+    scores = [95, 82, 67, 54, 100, 73]
+
+    print("All scores:", scores)
+    print("Class average:", average_score(scores))
+    print()
+
+    analyze_scores(scores)
